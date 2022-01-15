2969ff33e99c860ac1030b832e3e93bd3a595d87
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:20:54 -0300

    Update ex_112.pas

--- lista1/ex_112.pas
+++ lista1/ex_112.pas
@@ -1,3 +1,23 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia do teclado um número inteiro N e imprima se ele é PAR ou IMPAR. 
+
+Exemplos: 
+Entrada 1: 
+5 
+Saída Esperada 1: 
+IMPAR 
+
+Entrada 2: 
+3 
+Saída Esperada 2: 
+IMPAR 
+
+Entrada 3: 
+2 
+Saída Esperada 3: 
+PAR *) 
+
 program Ex_112;
 var N: integer;
 
