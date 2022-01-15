a654f7f807468361e7f2fb07ed39d468383b73d1
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:16:25 -0300

    Update ex_107.pas

--- lista1/ex_107.pas
+++ lista1/ex_107.pas
@@ -1,3 +1,23 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia de teclado um número inteiro e imprima se este é múltiplo de 3. 
+
+Exemplos: 
+Entrada 1: 
+5 
+Saída Esperada 1: 
+NAO 
+
+Entrada 2: 
+-3 
+Saída Esperada 2: 
+SIM 
+
+Entrada 3: 
+15 
+Saída Esperada 3: 
+SIM *)
+
 program Ex_107;
 var num: longint;
 
