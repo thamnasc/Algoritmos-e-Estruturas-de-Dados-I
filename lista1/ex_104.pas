31fcd6b0a69fea42bb5039b57c2ff6d9c451fa30
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:14:15 -0300

    Update ex_104.pas

--- lista1/ex_104.pas
+++ lista1/ex_104.pas
@@ -1,3 +1,25 @@
+(* Responder questão 'Exercicio 104' 
+
+Enunciado: 
+
+Faça um programa Pascal que leia do teclado dois números inteiros e imprima qual é o menor valor entre eles. 
+
+Exemplos: 
+Entrada 1: 
+5 4 
+Saída Esperada 1: 
+4 
+
+Entrada 2: 
+-3 -4 
+Saída Esperada 2: 
+-4 
+
+Entrada 3: 
+6 15 
+Saída Esperada 3: 
+6 *)
+
 program Ex_104;
 var A, B: longint;
 
