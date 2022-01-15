38ff253f110681b6142d9c9f1e72482466451b06
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:05:23 -0300

    Update ex_078.pas

--- lista1/ex_078.pas
+++ lista1/ex_078.pas
@@ -1,6 +1,7 @@
 (* Enunciado: 
 
-Considere que o número de uma placa de veículo é composto por quatro algarismos. Faça um programa Pascal que leia um número inteiro do teclado e imprima o algarismo correspondente à casa das centenas. Use os operadores DIV e MOD. 
+Considere que o número de uma placa de veículo é composto por quatro algarismos. Faça um programa Pascal que leia um número inteiro do teclado e imprima o 
+algarismo correspondente à casa das centenas. Use os operadores DIV e MOD. 
 
 Exemplos: 
 Entrada 1: 
