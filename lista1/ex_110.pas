1eb82c14179f9508851eda6d4d178439fdda0026
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:19:50 -0300

    Update ex_110.pas

--- lista1/ex_110.pas
+++ lista1/ex_110.pas
@@ -1,3 +1,24 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia dois números inteiros do teclado e imprima SIM se o primeiro 
+número é divisível pelo segundo. 
+
+Exemplos: 
+Entrada 1: 
+5 10 
+Saída Esperada 1: 
+NAO 
+
+Entrada 2: 
+4 2 
+Saída Esperada 2: 
+SIM 
+
+Entrada 3: 
+7 21 
+Saída Esperada 3: 
+NAO *)
+
 program Ex_110;
 var A, B: longint;
 
