236c79f020500275d4c7c7eb912079d5ffd488f3
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:02:30 -0300

    Update ex_075.pas

--- lista1/ex_075.pas
+++ lista1/ex_075.pas
@@ -1,3 +1,20 @@
+(*Enunciado: 
+
+Considere que o número de uma placa de veículo é composto por quatro algarismos. Faça um programa Pascal que leia um número inteiro do 
+teclado e imprima o algarismo correspondente à casa das unidades. 
+Use o operador MOD. 
+
+Exemplos: 
+Entrada 1: 
+2569 
+Saída Esperada 1: 
+9 
+
+Entrada 2: 
+1000 
+Saída Esperada 2: 
+0 *)
+
 program Ex_075;
 var placa, uni, de, cent: longint;
 
