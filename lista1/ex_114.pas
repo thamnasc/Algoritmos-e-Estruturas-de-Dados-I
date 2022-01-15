a01eb8ec830f1044b8cf4a8c14d7e16d763e666f
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:22:01 -0300

    Update ex_114.pas

--- lista1/ex_114.pas
+++ lista1/ex_114.pas
@@ -1,3 +1,24 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia um número inteiro do teclado, calcule se ele é ou não divisível por 5. 
+Imprima SIM caso ele seja e NAO em caso contrário. 
+
+Exemplos: 
+Entrada 1: 
+5 
+Saída Esperada 1: 
+SIM 
+
+Entrada 2: 
+-5 
+Saída Esperada 2: 
+SIM 
+
+Entrada 3: 
+3 
+Saída Esperada 3: 
+NAO *)
+
 program Ex_114;
 var num: longint;
 
