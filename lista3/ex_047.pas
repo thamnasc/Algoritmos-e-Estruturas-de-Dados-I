1b1b316a309b1396a31fe09f5d690491629b9f01
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:57:39 -0300

    Update ex_047.pas

--- lista3/ex_047.pas
+++ lista3/ex_047.pas
@@ -1,3 +1,11 @@
+(* Enunciado: 
+
+Faça um programa Pascal que calcule o valor da soma dos quadrados dos primeiros 50 inteiros positivos não nulos e imprima o resultado do cálculo na tela. 
+Observe que este programa não tem entrada, apenas saída. 
+
+1² + 2² + 3² + ... + 50²
+*)
+
 program ex_047;
 var i, square, sum: longint;
 
