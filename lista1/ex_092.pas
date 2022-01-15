131128273ca3f2e95a17335b3fb8f00aefcb650d
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:11:04 -0300

    Update ex_092.pas

--- lista1/ex_092.pas
+++ lista1/ex_092.pas
@@ -1,3 +1,24 @@
+(* Responder questão 'Exercicio 092' 
+ 
+Enunciado: 
+
+Faça um programa Pascal que leia três números inteiros (P1, P2 e P3) contendo as notas das três provas de um aluno em uma certa disciplina 
+e imprima a média final deste aluno, a qual deve ser um número inteiro. Considerar que a média é ponderada e que o peso das notas é 1, 2 e 3,
+respectivamente. A fórmula que calcula essa média é: 
+
+(P1 + 2 x P2 + 3 x P3 ) / (1 + 2 + 3)
+
+Exemplos: 
+Entrada 1: 
+10 50 80 
+Saída Esperada 1: 
+58 
+
+Entrada 2: 
+90 100 48 
+Saída Esperada 2: 
+72 *)
+
 program Exercicio_092;
 var P1, P2, P3, med: integer;
 
