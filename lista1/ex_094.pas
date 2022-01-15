35fbb7a97f2a34da3e9d4a08bce3df2b7b1e76d8
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:12:48 -0300

    Update ex_094.pas

--- lista1/ex_094.pas
+++ lista1/ex_094.pas
@@ -1,3 +1,20 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia dois números inteiros, um será o valor de um produto e outro o valor 
+de desconto que esse produto está recebendo. 
+Imprima quantos reais o produto custa na promoção. 
+
+Exemplos: 
+Entrada 1: 
+500 50 
+Saida Esperada 1: 
+450 
+
+Entrada 2: 
+60000 1 
+Saída Esperada 2: 
+59999 *)
+
 program Exercicio_094;
 var prod, desc: longint;
 
