3ab6b5f13cc67a6accd3dd78019431ad977acee8
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:31:02 -0300

    Update ex_022.pas

--- lista2/ex_022.pas
+++ lista2/ex_022.pas
@@ -1,3 +1,22 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia do teclado dois valores inteiros que são as coordenadas (X,Y) de um ponto no sistema cartesiano. 
+Imprima na tela o quadrante ao qual o ponto pertence: 1, 2, 3 ou 4, conforme as regras clássicas da matemática. 
+
+Caso o ponto não pertenca a nenhum quandrante, imprima X se ele está sobre o eixo X, Y, se ele está sobre o eixo Y, ou então imprima O, 
+caso ele esteja na origem. 
+
+Exemplos: 
+Exemplo de entrada 1: 
+4 4 
+Saida Esperada 1: 
+1 
+
+Exemplo de entrada 2: 
+4 0 
+Saida Esperada 2: 
+X  *)
+
 program Ex_022;
 var X, Y: longint;
 
