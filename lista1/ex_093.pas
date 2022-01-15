91dafb79b6e30db24794f71dc25c8ae681124b53
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:12:05 -0300

    Update ex_093.pas

--- lista1/ex_093.pas
+++ lista1/ex_093.pas
@@ -1,3 +1,18 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia um número inteiro e imprima o seu sucessor e seu antecessor, na mesma linha. 
+
+Exemplos: 
+Entrada 1: 
+1 
+Saída Esperada 1: 
+2 0 
+
+Entrada 2: 
+100 
+Saída Esperada 2: 
+101 99 *)
+
 program Exercicio_093;
 var num: integer;
 
