99951c091d56b4600aad097971e5c834b8198688
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:49:10 -0300

    Update ex_035.pas

--- lista2/ex_035.pas
+++ lista2/ex_035.pas
@@ -1,3 +1,35 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia do teclado um valor real que representa o salário mensal de uma pessoa. 
+Seu programa deve imprimir valor do imposto de renda (IR) mensal, em reais, de acordo com a tabela de 2009, que está abaixo. 
+
+Se o salário digitado for menor que o salário mínimo de R$ 540,00 o programa deve imprimir "NAO". 
+
+menor ou igual a 1424,00: 0%; 
+
+maior que 1424,00, menor ou igual a 2150,00: 7.5%; 
+
+maior que 2150,00, menor ou igual a 2866,00: 15%; 
+
+maior que 2866,00, menor ou igual a 3582,00: 22.5%; 
+
+maior que 3582,00: 27.5%. 
+
+Junto com o valor do IR mensal, o programa deve imprimir a Faixa (1,2,3,4 ou 5) correspondente ao salário. 
+
+Exemplos: 
+Entrada 1: 
+500.00 
+
+Saída Esperada 1: 
+NAO 
+
+Entrada 2: 
+2300.00 
+
+Saída Esperada 2: 
+3 345.00 *)
+
 program ex_035;
 var sal, IR: real;
 i: integer;
