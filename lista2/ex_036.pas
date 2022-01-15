a45a2fe64c37e25ac62838f0df7ab2421a1f0611
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:50:03 -0300

    Update ex_036.pas

--- lista2/ex_036.pas
+++ lista2/ex_036.pas
@@ -1,3 +1,26 @@
+(* Enunciado: 
+
+Alguém deseja cobrir as paredes de uma cozinha com azulejos. As lojas somente vendem caixas com 10 azulejos. 
+Todas as lojas do ramo vendem apenas 3 (três) tipos de azulejos, cujas dimensões são: 
+
+50cm x 40cm; 
+
+50cm x 60cm; 
+
+50cm x 80cm. 
+
+Faça um programa Pascal que leia do teclado dois valores inteiros representando respectivamente o tipo 
+do azulejo desejado (um dos números 1, 2 ou 3) e a área que se deseja azulejar, em metros quadrados. 
+Seu programa deve imprimir a quantidade de caixas de azulejos que deverão ser compradas para cobrir toda a área. 
+Considere que pedaços de azulejo podem ser reaproveitados, de maneira a minimizar a quantidade de caixas. 
+
+Exemplos: 
+Entrada 1: 
+2 122 
+
+Saída Esperada 1: 
+41 caixas *)
+
 program Ex_036;
 var tipo, area, caixa: real;
 
