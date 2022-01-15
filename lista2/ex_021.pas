0b2a169f2cc7d58930c82559a7ccfa9c8364e6c2
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:30:08 -0300

    Update ex_021.pas

--- lista2/ex_021.pas
+++ lista2/ex_021.pas
@@ -1,3 +1,31 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia do teclado um número inteiro positivo. 
+Esse número deve ser classificado em uma das seguintes situações: 
+
+Múltiplo exclusivamente de 7. 
+
+Múltiplo exclusivamente de 11. 
+
+Múltiplo de 7 e de 11. 
+
+Não é múltiplo nem de 7 nem de 11. 
+
+O programa deve imprimir a situação correspondente ao número lido. 
+
+Exemplos: 
+Entrada 1: 
+210 
+
+Saída Esperada 1: 
+Multiplo exclusivamente de 7. 
+
+Entrada 2: 
+200 
+
+Saída Esperada 2: 
+Nao e multiplo nem de 7 nem de 11. *)
+
 program Ex_021;
 var num: longint;
 
