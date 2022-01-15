cf68df5f792aa18b3a39ba61e96a0a302cad5e0a
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:07:31 -0300

    Update ex_087.pas

--- lista1/ex_087.pas
+++ lista1/ex_087.pas
@@ -1,3 +1,27 @@
+(* Enunciado: 
+
+Sabe-se que para iluminar de maneira correta os cômodos de uma casa, 
+para cada Metro quadrado (m2) deve-se usar 18W de potência. 
+
+Faça um programa Pascal que: 
+
+receba dois inteiros representando as duas dimensões de um comodo em metros; 
+
+calcule e imprima a sua área em m2; 
+
+imprima a potência de iluminação que deverá ser usada em watts. 
+
+Exemplos: 
+Entrada 1: 
+10 10 
+Saída Esperada 1: 
+100 1800 
+
+Entrada 2: 
+5 7 
+Saída Esperada 2: 
+35 630  *)
+
 program Exercicio_087;
 var alt, lar, pot: integer;
 
