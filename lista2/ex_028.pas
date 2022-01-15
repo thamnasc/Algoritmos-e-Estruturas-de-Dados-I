49a9d2ae5f8d09f206f4cc3e158fed610fee4b75
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:35:54 -0300

    Update ex_028.pas

--- lista2/ex_028.pas
+++ lista2/ex_028.pas
@@ -1,3 +1,27 @@
+(* Enunciado: 
+
+Sabendo-se que a água se solidifica a zero grau Celsius, ou a 32 Fahrenheit, e que entra em ebulição a 100 graus Celsius ou 212 Fahrenheit,
+faça um programa Pascal que obtenha do teclado um inteiro que é o valor de temperatura em Fahrenheit e imprima na tela o valor 
+correspondente em Celsius e uma mensagem indicando se a água nesta temperatura está no estado liquido, sólido ou gasoso. A fórmula de conversão entre graus 
+Celsius e Farenheit é: 
+
+5F - 9C - 160 = 0
+
+Exemplos: 
+Entrada 1: 
+45 
+
+Saída Esperada 1: 
+7.22 
+liquido 
+
+Entrada 2: 
+240 
+
+Saída Espearada 2: 
+115.55 
+gasoso *)
+
 program Ex_028;
 var F, C: real;
 
