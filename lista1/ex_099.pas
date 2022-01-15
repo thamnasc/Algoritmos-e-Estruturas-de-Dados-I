0e9a3afc4cc9bfe766d1756ac2520bf6b85e48e8
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:13:37 -0300

    Update ex_099.pas

--- lista1/ex_099.pas
+++ lista1/ex_099.pas
@@ -1,3 +1,22 @@
+(* Responder questão 'Exercicio 099' 
+
+Enunciado: 
+
+Faça um programa Pascal que leia três números inteiros representando a idade de uma pessoa expressa em anos, 
+meses e dias, respectivamente, e imprima-a expressa apenas em dias. Para este exercício, considere que todos 
+os meses possuem 30 dias e desconsidere anos bissextos. 
+
+Exemplos: 
+Entrada 1: 
+12 4 18 
+Saída Esperada 1: 
+4518 
+
+Entrada 2: 
+30 2 1 
+Saída Esperada 2: 
+11011 *)
+
 program Exercicio_099;
 var anos, meses, dias, days: longint;
 
