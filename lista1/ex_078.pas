1d37e3508d59bbb0210e2eb153e4ad1ae201524c
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:03:58 -0300

    Update ex_078.pas

--- lista1/ex_078.pas
+++ lista1/ex_078.pas
@@ -1,3 +1,23 @@
+(* Enunciado: 
+
+Considere que o número de uma placa de veículo é composto por quatro algarismos. Faça um programa Pascal que leia um número inteiro do teclado e imprima o algarismo correspondente à casa das centenas. Use os operadores DIV e MOD. 
+
+Exemplos: 
+Entrada 1: 
+2500 
+Saída Esperada 1: 
+5 
+
+Entrada 2: 
+2031 
+Saída Esperada 2: 
+0 
+
+Entrada 3: 
+6975 
+Saída Esperada 3: 
+9 *)
+
 program Ex_078;
 var placa, mil: longint;
 
