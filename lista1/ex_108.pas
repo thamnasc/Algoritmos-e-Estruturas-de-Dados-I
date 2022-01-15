3e21cf09d27aaa7eaaa3bc90abe835dbd09b69be
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:18:30 -0300

    Update ex_108.pas

--- lista1/ex_108.pas
+++ lista1/ex_108.pas
@@ -1,3 +1,24 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia um número inteiro do teclado e indique se ele está compreendido 
+entre 20 e 90 ou não (20 e 90 não estão na faixa de valores). 
+
+Exemplos: 
+Entrada 1: 
+50 
+Saída Esperada 1: 
+SIM 
+
+Entrada 2: 
+20 
+Saída Esperada 2: 
+NAO 
+
+Entrada 3: 
+90 
+Saída Esperada 3: 
+NAO *)
+
 program Ex_108;
 var num: integer;
 
