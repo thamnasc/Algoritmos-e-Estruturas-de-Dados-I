ba1734278b288bb3113630e2e52d27de3b04c17a
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:47:44 -0300

    Update ex_032.pas

--- lista2/ex_032.pas
+++ lista2/ex_032.pas
@@ -1,3 +1,37 @@
+(* Enunciado: 
+
+Uma empresa concederá um aumento de salário aos seus funcionários, mas este aumento será de acordo com o cargo que cada um ocupa. 
+A tabela abaixo contém os códigos, o cargo e e percentual de aumento correspondente. 
+Faça um programa Pascal que leia dois valores do teclado, o primeiro é o salário de um funcionário e o segundo é o código do cargo dele. 
+Calcule o valor do novo salário. 
+Se o cargo do funcionário não estiver na tabela, ele deverá receber 40% de aumento. Imprima o valor do salário antigo, 
+o do novo salário e a diferença entre eles, nesta ordem, em 3 linhas. 
+
+Código      Cargo           Percentual 
+
+101         Gerente         10% 
+
+102         Engenheiro      20% 
+
+103         Técnico         30% 
+
+Exemplos: 
+Exemplo 1: 
+2500 101 
+
+Saida Esperada 1: 
+2500.00 
+2750.00 
+250.00 
+
+Exemplo 2: 
+5000 102 
+
+Saida Esperada 2: 
+5000.00 
+6000.00 
+1000.00 *)
+
 program Ex_032;
 var sal, cod, novo: real;
 
