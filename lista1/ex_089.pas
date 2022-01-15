81f0052db85340ba53dd8afcb52431fa02daf962
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:08:40 -0300

    Update ex_089.pas

--- lista1/ex_089.pas
+++ lista1/ex_089.pas
@@ -1,3 +1,28 @@
+(* Enunciado: 
+
+O custo ao consumidor de um carro novo é a soma do custo de fábrica com a percentagem do distribuidor
+e dos impostos (aplicados ao custo de fábrica). 
+Supondo que a percentagem do distribuidor seja de 28% e os impostos de 45%, faça um programa Pascal que 
+leia um número inteiro representando o custo de fábrica de um carro e imprima o custo ao consumidor. 
+
+PS: Considere sempre o arredondamento com Truncamento. 
+
+Exemplos: 
+Entrada 1: 
+15000 
+Saída Esperada 1: 
+25950 
+
+Entrada 2: 
+12500 
+Saída Esperada 2: 
+21625 
+
+Entrada 3: 
+8350 
+Saída Esperada 3: 
+14445 *)
+
 program Exercicio_089;
 var custof, custoc: longint;
 
