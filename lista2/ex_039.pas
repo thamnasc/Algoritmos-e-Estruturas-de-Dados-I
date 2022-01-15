e3538a4078541fca20b20648cfa928faacd1bd34
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:51:10 -0300

    Update ex_039.pas

--- lista2/ex_039.pas
+++ lista2/ex_039.pas
@@ -1,3 +1,19 @@
+(* Enunciado: 
+
+Faça um programa Pascal que receba um número inteiro positivo N e calcule a produto dos N primeiros números pares positivos. 
+Ao final, imprima este produto. Para esse exercício, considere o primeiro número par como sendo 2. 
+
+Exemplos: 
+Entrada 1: 
+2 
+Saída Esperada 1: 
+8 
+
+Entrada 2: 
+4 
+Saída Esperada 2: 
+384  *)
+
 program ex_39;
 var num, par, N, i: longint;
 
