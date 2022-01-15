6129d3fa6dbdd3df6555ee273ed001431e92ece4
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:19:07 -0300

    Update ex_109.pas

--- lista1/ex_109.pas
+++ lista1/ex_109.pas
@@ -1,3 +1,24 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia um número inteiro do teclado. Se ele estiver entre os valores 
+-15 e 30 (-15 e 30 não estão inclusos), imprima seu número oposto, senão imprima o próprio número. 
+
+Exemplos: 
+Entrada 1: 
+50 
+Saída Esperada 1: 
+50 
+
+Entrada 2: 
+-10 
+Saída Esperada 2: 
+10 
+
+Entrada 3: 
+23 
+Saída Esperada 3: 
+-23 *)
+
 program Ex_109;
 var num: integer;
 
