8345a64c9f7e4d421643affe0c20dd4c720045c7
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:22:35 -0300

    Update ex_116.pas

--- lista1/ex_116.pas
+++ lista1/ex_116.pas
@@ -1,3 +1,25 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia do teclado um número inteiro e imprima SIM caso o número seja impar, 
+negativo e menor que -20 ou então se for par, positivo e maior que 7. Caso contrário imprima NAO. 
+A dica é usar uma combinação correta que envolva os operadores AND e OR. 
+
+Exemplos: 
+Entrada 1: 
+17 
+Saída Esperada 1: 
+NAO 
+
+Entrada 2: 
+-101 
+Saída Esperada 2: 
+SIM 
+
+Entrada 3: 
+-13 
+Saída Esperada 3: 
+NAO *)
+
 program Ex_116;
 var num: integer;
 
