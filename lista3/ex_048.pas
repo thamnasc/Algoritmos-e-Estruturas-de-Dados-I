d7319648bf8c8f8c7686722a3b3029bc43a29f4e
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:58:43 -0300

    Update ex_048.pas

--- lista3/ex_048.pas
+++ lista3/ex_048.pas
@@ -1,3 +1,23 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia do teclado dois números positivos ímpares A e B onde (A <= B) e calcule o produto dos números ímpares de A até B. 
+Isto é, calcule: 
+
+A x (A + 2) x (A + 4) x ... x B
+
+Imprima "erro" caso o número lido não satisfaça as condições. Caso contrário imprima o resultado do cálculo. 
+
+Exemplos: 
+Entrada 1: 
+3 5 
+Saida Esperada 1: 
+15 
+
+Entrada 2: 
+7 15 
+Saida Esperada 2: 
+135135 *)
+
 program ex_048;
 var A, B, prod: longint;
 
