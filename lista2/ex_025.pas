b9d16ac82a4fe432b04081e60511839e5ffebfad
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:32:25 -0300

    Update ex_025.pas

--- lista2/ex_025.pas
+++ lista2/ex_025.pas
@@ -1,3 +1,22 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia do teclado três valores inteiros representando a data de nascimento de uma pessoa (dia, mês e ano) 
+e imprima quantos anos completos ela terá no dia 29/04/2021. 
+
+Exemplos: 
+
+Entrada 1: 
+17 05 1988 
+
+Saída Esperada 1: 
+32 
+
+Entrada 2: 
+29 04 2021 
+
+Saída Esperada 2: 
+0 *)
+
 program Ex_025;
 var dia, mes, ano, anos: integer;
 
