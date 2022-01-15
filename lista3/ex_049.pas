ce7ea1bf6d256189ce026f5397a1a729faddfe4f
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:59:20 -0300

    Update ex_049.pas

--- lista3/ex_049.pas
+++ lista3/ex_049.pas
@@ -1,3 +1,43 @@
+(* Enunciado: 
+
+Uma agência governamental deseja conhecer a distribuição da população do país por faixa salarial. Para isto, coletou dados do último censo 
+realizado e criou um arquivo contendo, em cada linha, o salário de um cidadão particular. Os salários variam de zero a 19.000,00 unidades da moeda local. 
+
+Considere o salário mínimo igual a 450,00 unidades da moeda local. 
+
+As faixas salariais de interesse são as seguintes: 
+
+de 0 a 3 salários mínimos 
+
+de 4 a 9 salários mínimos 
+
+de 10 a 20 salários mínimos 
+
+acima de 20 salários mínimos. 
+
+Faça um programa Pascal que leia uma sequência de números reais, que será terminado em zero. O zero não deve ser processado e serve para terminar o 
+programa. O programa deve imprimir na tela os percentuais da população para cada faixa salarial de interesse. 
+
+Exemplos: 
+Entrada 1: 
+240.99 
+2720.77 
+4560.88 
+19843.33 
+834.15 
+315.87 
+5645.80 
+150.33 
+2560.00 
+2490.05 
+0.00 
+
+Saída Esperada 1: 
+40.00 
+30.00 
+20.00 
+10.00 *)
+
 program ex_049;
 var sal, div1, div2, div3, div4, sum: real;
 const min = 450;
