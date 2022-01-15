303e6f34600f33e1b757344dc8885e8e49037344
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:15:28 -0300

    Update ex_106.pas

--- lista1/ex_106.pas
+++ lista1/ex_106.pas
@@ -1,3 +1,25 @@
+(* Responder questão 'Exercicio 106' 
+
+Enunciado: 
+
+Faça um programa Pascal que leia um número inteiro do teclado e imprima o cubo do número caso ele seja positivo ou igual a zero e o quadrado do número caso ele seja negativo. 
+
+Exemplos: 
+Entrada 1: 
+0 
+Saída Esperada 1: 
+0 
+
+Entrada 2: 
+4 
+Saída Esperada 2: 
+64 
+
+Entrada 2: 
+-5 
+Saída Esperada 2: 
+25 *)
+
 program Ex_106;
 var num: integer;
 
