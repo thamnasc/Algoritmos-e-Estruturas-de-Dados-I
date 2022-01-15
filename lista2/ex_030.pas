df06788fb6a2eceb2f07bca7aa8683145c845d28
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:45:22 -0300

    Update ex_030.pas

--- lista2/ex_030.pas
+++ lista2/ex_030.pas
@@ -1,3 +1,32 @@
+(* Enunciado: 
+
+Um vendedor necessita de um programa que calcule o preço total devido por um cliente que comprou um produto em sua loja. 
+Faça um programa Pascal que receba dois números inteiros que são, respectivamente, o código do produto e a quantidade comprada. 
+Imprima na tela o preço total, usando a tabela abaixo. 
+Caso o código não exista o programa deve imprimir ERRO. 
+
+Código do Produto               Preço unitário
+1001                            5,32 
+
+1324                            6,45
+
+6548                            2,37
+
+987                             5,32 
+
+7623                            6,45 
+
+Exemplos: 
+Exemplo 1: 
+1324 6 
+Saida Esperada 1: 
+38.70 
+
+Exemplo 2: 
+987 9 
+Saida Esperada 2: 
+47.88  *)
+
 program Ex_030;
 var     cod, quant: integer;
 total: real;
