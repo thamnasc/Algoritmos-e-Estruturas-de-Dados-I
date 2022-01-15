84eb2518891705ae47c3230cf1cc0a42fdfcc898
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:52:49 -0300

    Update ex_041.pas

--- lista2/ex_041.pas
+++ lista2/ex_041.pas
@@ -1,3 +1,21 @@
+(* Enunciado: 
+
+Faça um programa Pascal que receba um número positivo N e imprima na tela a soma dos N primeiros números da sequência de Fibonacci. 
+Os dois primeiros números da sequência são 0 e 1, e os próximos são dados pela soma dos dois últimos números anteriormente calculados. 
+
+A título de exemplo, os oito primeiros valores dessa sequência são: 0, 1, 1, 2, 3, 5, 8, 13. 
+
+Exemplos: 
+Entrada 1: 
+3 
+Saída Esperada 1: 
+2 
+
+Entrada 2: 
+5 
+Saída Esperada 2: 
+7 *)
+
 program ex_041;
 var penult, ult, aux, i, N, soma: longint;
 
