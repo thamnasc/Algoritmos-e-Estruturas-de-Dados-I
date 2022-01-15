c669ee2a700e2d0e4c7c3466a1a51ba73cf7cbc6
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:31:46 -0300

    Update ex_024.pas

--- lista2/ex_024.pas
+++ lista2/ex_024.pas
@@ -1,3 +1,30 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia do teclado um valor inteiro representando o ano de nascimento de uma pessoa. 
+Com esse dado, o programa deve fazer o seguinte: 
+
+Calcular e imprimir sua idade, considerando que estamos no ano de 2020; 
+
+Verificar e imprimir se a pessoa já tem idade para votar (16 anos ou mais); 
+
+Verificar e imprimir se a pessoa já tem idade para conseguir a carteira de habilitação (18 ou mais). 
+
+Exemplos: 
+Entrada 1: 
+2002 
+Saida Esperada 1: 
+18 
+ 
+SIM 
+SIM 
+
+Entrada 2: 
+2005 
+Saida Esperada 2: 
+15 
+NAO 
+NAO  *)
+
 program Ex_024;
 var ano, idade: longint;
 
