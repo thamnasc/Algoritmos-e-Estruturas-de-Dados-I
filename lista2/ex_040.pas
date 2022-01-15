da07445fc94bc7b284a18431c596836fd0bf6ab4
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:51:57 -0300

    Update ex_040.pas

--- lista2/ex_040.pas
+++ lista2/ex_040.pas
@@ -1,3 +1,21 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia uma sequência de números inteiros terminada em zero e imprima o maior e o menor número dessa sequência. 
+O valor zero não deve ser ser processado, ele serve para marcar o final da entrada de dados. 
+
+Exemplos: 
+Entrada 1: 
+1 55 30 -2 560 -1 0 
+
+Saída Esperada 1: 
+560 -2 
+
+Entrada 2: 
+-3 -4 -30 -10 0 
+
+Saída Esperada 2: 
+-3 -30 *)
+
 program ex_040;
 var maior, menor, num: longint;
 
