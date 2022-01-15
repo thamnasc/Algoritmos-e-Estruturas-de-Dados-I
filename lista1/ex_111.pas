98c2292915384d90b1955b287d9ba196e563b75f
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 20:20:21 -0300

    Update ex_111.pas

--- lista1/ex_111.pas
+++ lista1/ex_111.pas
@@ -1,3 +1,23 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia um número, calcule se ele é divisível por 3 e por 7. Caso seja, imprima SIM e caso não seja imprima NAO. Dica: use o operador AND. 
+
+Exemplos: 
+Entrada 1: 
+21 
+Saída Esperada 1: 
+SIM 
+
+Entrada 2: 
+7 
+Saída Esperada 2: 
+NAO 
+
+Entrada 2: 
+-3 
+Saída Esperada 2: 
+NAO *)
+
 program Ex_111;
 var num: longint;
 
