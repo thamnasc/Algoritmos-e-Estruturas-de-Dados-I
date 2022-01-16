74d5f0fac576c3fb513058f619760c4cf5b264ce
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 22:38:33 -0300

    Update series_005.pas

--- lista5_séries/series_005.pas
+++ lista5_séries/series_005.pas
@@ -1,3 +1,13 @@
+(* Enunciado: 
+
+Observe a soma infinita abaixo. Ela é formada por frações em que cada numerador é o dobro do denominador da fração anterior e cada denominador, 
+por sua vez, é o dobro do numerador da fração anterior (exceto a primeira fração). 
+
+S = (1/3) + (6/2) + (4/12) + (24/8) + (16/48) + ...
+
+Faça um programa em Pascal para calcular o valor de S, considerando apenas os 10 primeiros termos da série. Ao final, imprimir o resultado encontrado 
+para S com duas casas após a vírgula.  *)
+
 program series_005;
 var n: longint;
 
