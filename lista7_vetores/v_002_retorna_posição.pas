30d1758ebbdc895834ac7c8ab88e478c4253dadf
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 23:07:06 -0300

    Update and rename v_002.pas to v_002_retorna_posição.pas

--- lista7_vetores/v_002_retorna_posição.pas
+++ lista7_vetores/v_002_retorna_posição.pas
@@ -1,4 +1,25 @@
-program v_003;
+(*Enunciado: 
+
+Faça um programa que leia uma sequência de inteiros terminada em zero e armazene esta sequência em um vetor. 
+O zero não deve ser processado pois serve para marcar o final da entrada de dados. Em seguida o programa deve 
+ler uma outra sequência de inteiros também terminada em zero e, para cada valor lido, o programa deve dizer qual 
+a posição do valor dentro do vetor, caso o valor exista no vetor, ou 0 (zero) caso não exista. Esta segunda sequência 
+não precisa ser armazenada em vetores. Use ao máximo funções e procedimentos apropriados. 
+
+Exemplo de entrada 
+-2 -6 7 2 0 
+ 
+7 
+3 
+2 
+0 
+
+Saída esperada 
+3 
+0 
+4 *)
+
+program v_002;
 const MIN = 1; MAX = 200;
 type vetor = array [MIN..MAX] of longint;
 var seq: vetor;
