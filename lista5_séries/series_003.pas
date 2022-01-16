6bf97e3171f57d1c1151cdc52891d47f16cf3b15
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 22:35:43 -0300

    Update series_003.pas

--- lista5_séries/series_003.pas
+++ lista5_séries/series_003.pas
@@ -1,4 +1,12 @@
+(* Enunciado: 
 
+Observe a soma infinita abaixo. Ela é formada por frações em que cada numerador é a soma entre o numerador e o denominador da fração 
+anterior e cada denominador, por sua vez, é a soma do seu numerador com o denominador da fração anterior (exceto a primeira fração). 
+
+S = (1/1) + (2/3) + (5/8) + (13/21) + (34/55) + ...
+
+Faça um programa em Pascal que calcula o valor de S, considerando apenas os 5 primeiros termos da série. Ao final, imprimir o resultado 
+encontrado para S com duas casas decimais. *)
 
 program series_003;
 const MIN = 1; MAX = 200;
