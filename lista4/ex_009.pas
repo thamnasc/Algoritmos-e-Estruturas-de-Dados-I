c6ad09d2c4e8844b58c0ce8b691e9aeb8656909e
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 22:24:03 -0300

    Update ex_009.pas

--- lista4/ex_009.pas
+++ lista4/ex_009.pas
@@ -1,3 +1,21 @@
+(* Enunciado: 
+
+Dadas as populações Pa e Pb de duas cidades A e B (no ano atual) e suas respectivas taxas de crescimento anual Ta e Tb, 
+faça um programa Pascal que que receba 4 números reais como entrada (Pa, Pb, Ta, Tb) representando estas informações e 
+determine se a população da cidade de menor população ultrapassará a de maior população e se sim, imprima em quantos anos 
+que isto ocorrerá. Caso contrário, imprima 0. 
+
+Exemplos: 
+Entrada 1: 
+300 500 0.12 0.05 
+Saída Esperada 1: 
+8 
+
+Entrada 2: 
+300 500 0.1 0.5 
+Saída Esperada 2: 
+0 *)
+
 program ex_009;
 var Pa, Pb, Ta, Tb: real;
 anos: integer;
