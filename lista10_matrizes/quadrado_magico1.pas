f2b33d280e8db325d7ca9c9111880f0c829a5c52
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 23:38:07 -0300

    Update quadrado_magico1.pas

--- lista10_matrizes/quadrado_magico1.pas
+++ lista10_matrizes/quadrado_magico1.pas
@@ -1,3 +1,27 @@
+(* Enunciado: 
+
+Dizemos que uma matriz quadrada inteira é um quadrado mágico se a soma dos elementos de cada linha, 
+a soma dos elementos de cada coluna e a soma dos elementos das diagonais principal e secundária são todos iguais. 
+Exemplo: 
+
+8 0 7 
+4 5 6 
+3 10 2 
+
+é um quadrado mágico pois 8+0+7 = 4+5+6 = 3+10+2 = 8+4+3 = 0+5+10 = 7+6+2 = 8+5+2 = 3+5+7 = 15. 
+
+Crie um programa em Free Pascal que leia um valor n representando o tamanho da matriz e leia uma matrix 
+A(n x n) que representa o quadrado e informe se a matriz é um quadrado mágico. 
+
+Exemplo de entrada: 
+3 
+8 0 7 
+4 5 6 
+3 10 2 
+
+Saída esperada para o exemplo acima: 
+sim *)
+
 program quadrado_magico_1;
 const MIN = 1; MAX = 100;
 type matriz = array [MIN..MAX, MIN..MAX] of longint;
