8f47f378945d6a0f86b264489e6a2b9528179298
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 21:04:47 -0300

    Update ex_061.pas

--- lista3/ex_061.pas
+++ lista3/ex_061.pas
@@ -1,3 +1,21 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia um número inteiro positivo N do teclado. Depois disso, o programa deve calcular e imprimir todos os 
+arranjos de dois números inteiros positivos A, B, ambos menores que N, de forma que quando somados (A + B), resultam no número N. Cada arranjo 
+A, B deve ser impresso em uma linha de saída. 
+
+Exemplos: 
+Entrada 1: 
+7 
+
+Saida Esperada 1: 
+1 6 
+2 5 
+3 4 
+4 3 
+5 2 
+6 1 *)
+
 program ex_061;
 var N, A, B: integer;
 
