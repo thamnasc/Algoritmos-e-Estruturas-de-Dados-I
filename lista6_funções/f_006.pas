3d9ac1149e6f4a8e704964be9bc258dd58f59ae7
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 22:56:46 -0300

    Update f_006.pas

--- lista6_funções/f_006.pas
+++ lista6_funções/f_006.pas
@@ -1,3 +1,25 @@
+(* Enunciado: 
+
+Faça uma função que receba como parâmetros seis inteiros dia1, mes1, ano1, dia2, mes2, ano2 , todas do tipo integer. 
+Considerando que cada trinca de dia, mês e ano representa uma data, a função deve retornar true se a primeira data for 
+anterior à segunda e false caso contrário. Teste sua função usando o código abaixo. 
+
+
+program compara datas;
+var dia1, mes1, ano1, dia2, mes2, ano2: longint;
+
+//coloque aqui o codigo da sua funcao que compara as datas
+
+begin
+	read(dia1, mes1, ano1, dia2, mes2, ano2);
+	if(eh_anterior(dia1, mes1, ano1, dia2, mes2, ano2)) then
+		writeln('a primeira data eh anterior')
+	else
+		writeln('a primeira data nao eh anterior');
+end.
+
+ *)
+ 
 program f_006;
 var dia1, mes1, ano1, dia2, mes2, ano2: longint;
 
