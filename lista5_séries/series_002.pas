b420afc7bc866b9ac5335c7c7fc5cc318a8e50ab
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 22:33:56 -0300

    Update series_002.pas

--- lista5_séries/series_002.pas
+++ lista5_séries/series_002.pas
@@ -1,3 +1,25 @@
+(* Enunciado: 
+
+Faça um programa em Pascal que lê do teclado um número inteiro positivo N. Depois disso, o programa deve calcular e imprimir a soma de todas 
+as frações em que a soma do numerador com o denominador de cada fração seja o número N. 
+
+Por exemplo, se N=7, o programa deve calcular a soma abaixo: 
+
+S = (1/6) + (2/5) + (3/4) + (4/3) + (5/2) + (6/1)
+
+a saída com duas casas decimais. 
+
+Exemplo: 
+Entrada 1: 
+7 
+Saída Esperada 1: 
+11.15 
+
+Entrada 2: 
+4 
+Saída Esperada 2: 
+4.33 *)
+
 program series_002;
 const MIN = 1; MAX = 200;
 type vetor = array [MIN..MAX] of longint;
