a201724e0fd28b056b0b344990b092685874d34b
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 21:01:44 -0300

    Update ex_052.pas

--- lista3/ex_052.pas
+++ lista3/ex_052.pas
@@ -1,3 +1,20 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia do teclado um número inteiro m e em seguida uma sequência de n números reais. Imprima a média aritmética inteira deles. 
+
+(N1 + N2  + ... + Nm) / m
+
+Exemplos: 
+Entrada 1: 
+2 2 4 
+Saida Esperada 1: 
+3 
+
+Entrada 2: 
+5 8 9 6 5 7 
+Saida Esperada 2: 
+7 *)
+
 program ex_052;
 var m, N, i, soma: longint;
 
