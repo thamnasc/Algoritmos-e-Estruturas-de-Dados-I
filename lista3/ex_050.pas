874114f1b07caad84995735f408562fa50416306
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 21:00:14 -0300

    Update ex_050.pas

--- lista3/ex_050.pas
+++ lista3/ex_050.pas
@@ -1,3 +1,15 @@
+(* Enunciado: 
+
+Faça um programa Pascal que, dados dois números inteiros positivos, imprima quantas vezes o primeiro divide exatamente o segundo. 
+Se o primeiro não divide o segundo o número de vezes é zero. Por exemplo, 72 pode ser dividido exatamente por 3 duas vezes. Use operadores inteiros apenas. 
+
+Exemplos: 
+Entrada 1: 
+72 3 
+
+Saída Esperada 1: 
+2 *)
+
 program Ex_050;
 var A, B, cont: integer;
 
