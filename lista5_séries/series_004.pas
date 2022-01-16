f13fa7e71bfc7c8d7b6afbb304d66abbc88d7825
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 22:37:17 -0300

    Update series_004.pas

--- lista5_séries/series_004.pas
+++ lista5_séries/series_004.pas
@@ -1,4 +1,14 @@
-program series_005;
+(* Enunciado: 
+
+Observe a soma infinita abaixo. Ela é formada por frações em que o numerador e o denominador são os valores sucessores dos valores do 
+numerador e do denominador da frações anterior, porém, alternadamente invertidos (exceto a primeira fração). 
+
+S = (1/2) + (4/3) + (5/6) + (8/7) + (9/10) + (12/11) + ... 
+
+Faça um programa em Pascal para calcular o valor de S, considerando apenas os 10 primeiros termos da série. Ao final, imprimir o resultado 
+encontrado para S com 2 casas após a vírgula. *)
+
+program series_004;
 var n: longint;
 
 function soma(n: longint): real;
