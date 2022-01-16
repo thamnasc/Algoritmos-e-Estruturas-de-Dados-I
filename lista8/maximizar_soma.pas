ce5fda43b21c18328fe04a6dca2a2eb56c9098af
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 23:16:49 -0300

    Update maximizar_soma.pas

--- lista8/maximizar_soma.pas
+++ lista8/maximizar_soma.pas
@@ -1,3 +1,20 @@
+(* Enunciado: 
+
+Faça um programa em Free Pascal que leia um número natural 0 < n ≤ 100 e em seguida leia uma sequência de n números inteiros. 
+Seu programa deve determinar o valor do subsequência que maximize a soma dos seus elementos. 
+
+Exemplo de entrada: 
+12 
+5 2 -2 -7 3 14 10 -3 9 -6 4 1 
+
+Saı́da esperada para a entrada acima: 
+33 
+
+Observação 1: O valor 33 da saı́da esperada acima, foi obtido pela soma dos valores dos ı́ndices de 5 a 9, isto é, v[5] = 3 até v[9] = 9. 
+
+Observação 2: Idealmente, seu programa deve fazer o menor número possı́vel de somas dos elementos dos vetores, embora o FARMA-ALG apenas checa 
+se sua resposta está correta. Você consegue fazer um programa que nunca some duas vezes uma sequência que já foi somada antes? Teste isso fora do FARMA-ALG. *)
+
 program maximizar_soma;
 const MIN = 1; MAX = 100;
 type vetor = array [MIN..MAX] of longint;
