3bbd6443b15312435387ba655d4380981f3d9d8c
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 22:26:44 -0300

    Update ex_013.pas

--- lista4/ex_013.pas
+++ lista4/ex_013.pas
@@ -1,3 +1,25 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia do teclado três números inteiros positivos: i, j e k. Se j for múltiplo de i e k for múltiplo de j, 
+o programa deve imprimir a soma dos três. Se os três valores forem consecutivos na ordem lida, o programa deve imprimi-los na ordem decrescente. 
+Em qualquer outra situação, o programa deve calcular e imprimir a média aritmética simples dos três valores. 
+
+Exemplos: 
+Entrada 1: 
+33 165 495 
+Saída Esperada 1: 
+693 
+
+Entrada 2: 
+74 75 76 
+Saída Esperada 2: 
+76 75 74 
+
+Entrada 3: 
+7 20 12 
+Saída Esperada 3: 
+13 *)
+
 program ex_013;
 var i, j, k: longint;
 	
