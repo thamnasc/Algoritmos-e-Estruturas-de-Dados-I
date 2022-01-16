98e4810c9197f193f3eb859104068355363c1342
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 21:02:52 -0300

    Update ex_055.pas

--- lista3/ex_055.pas
+++ lista3/ex_055.pas
@@ -1,3 +1,21 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia dois números n e outro m (0 <= n <= 9) e conte quantos dígitos n existem em m. 
+Se não existir nenhum dígito correspondente, a mensagem "NAO" deve ser exibida. Caso contrário imprima o resultado do seu cálculo. 
+
+Exemplos: 
+Entrada 1: 
+9 
+95949 
+Saída Esperada 1: 
+3 
+
+Entrada 2: 
+1 
+2353 
+Saída Esperada 2: 
+NAO *)
+
 program ex_055;
 var m: longint;
 n, cont: integer;
