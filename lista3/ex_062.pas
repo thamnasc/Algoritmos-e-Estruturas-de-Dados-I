225e4b8e395ca18fa3eab082497ad7621bb50de5
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 21:05:15 -0300

    Update ex_062.pas

--- lista3/ex_062.pas
+++ lista3/ex_062.pas
@@ -1,3 +1,21 @@
+(* Enunciado: 
+
+Faça um programa Pascal que receba uma sequência de números reais terminada em zero representando o saldo bancário de alguns 
+clientes de um banco e imprima aqueles que são negativos. O valor zero serve para indicar o final da entrada de dados e não deve ser processado. 
+
+Exemplos: 
+Entrada 1: 
+832.47 
+215.25 
+-1987.11 
+19.00 
+-45.38 
+0 
+
+Saída Esperada 1: 
+-1987.11 
+-45.38 *)
+
 program ex_062;
 var sal: real;
 
