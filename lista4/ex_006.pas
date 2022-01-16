b5b6d378379eea1891850bbda8ad81b69c0b2e5f
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 22:21:03 -0300

    Update ex_006.pas

--- lista4/ex_006.pas
+++ lista4/ex_006.pas
@@ -1,3 +1,22 @@
+(* Enunciado: 
+
+Um inteiro positivo N é considerado perfeito se o mesmo for igual a soma de seus divisores positivos diferentes de N. 
+
+Exemplo: 6 é perfeito pois 1 + 2 + 3 = 6 e 1, 2 e 3 são todos os divisores positivos de 6 e que são diferentes de 6. 
+
+Faça um programa Pascal que leia um número inteiro positivo K e mostre os K primeiros números que são perfeitos. 
+
+Exemplos: 
+Entrada 1: 
+1 
+Saída Esperada 1: 
+6 
+
+Entrada 2: 
+2 
+Saída Esperada 2: 
+6 28 *)
+
 program ex_006;
 var K, cont, perfeito: longint;
 
