d5986e405e04a458a0edcc0dc54c088cf47c6d9b
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 22:58:13 -0300

    Update f_007.pas

--- lista6_funções/f_007.pas
+++ lista6_funções/f_007.pas
@@ -1,3 +1,25 @@
+(* Enunciado: 
+
+Faça uma procedure que receba como parâmetro um inteiro e retorne este número incrementado de uma unidade. 
+Use esta procedure para fazer funcionar o código abaixo, que imprime todos os números de 1 a 10. 
+Teste sua procedure usando o código abaixo. 
+
+
+program incrementa uma unidade;
+var n: longint;
+
+//coloque aqui o codigo da sua procedure que incrementa uma unidade
+
+begin
+	n:= 1;
+	while n <= 10 do
+	begin
+		writeln(n);
+		incrementa(n);
+	end;
+end.
+ *)
+ 
 program f_007;
 var n: longint;
 
