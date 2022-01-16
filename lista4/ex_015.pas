648032716c33c9eacf3a7ff94f99d0bfbc803bb6
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 22:27:46 -0300

    Update ex_015.pas

--- lista4/ex_015.pas
+++ lista4/ex_015.pas
@@ -1,3 +1,22 @@
+(* Enunciado: 
+
+Se multiplicarmos 37 por alguns números, obteremos números cujos algarismos, quando somados, resultam no mesmo número que foi multiplicado pelo 37. 
+Por exemplo, se tomarmos o número 15, multiplicando-o por 37, obtemos 555. Somando-se 5 + 5 + 5 resulta em 15. 
+
+Faça um programa Pascal que leia um número inteiro positivo do teclado, calcule o resultado da multiplicação por 37, some os algarismos do resultado, 
+compare essa soma com o número lido e imprima "SIM" se há coincidência ou "NAO" se não há coincidência. 
+
+Exemplos: 
+Entrada 1: 
+15 
+Saída Esperada 1: 
+SIM 
+
+Entrada 2: 
+26 
+Saída Esperada 2: 
+NAO *)
+
 program ex_015;
 var num: longint;
 
