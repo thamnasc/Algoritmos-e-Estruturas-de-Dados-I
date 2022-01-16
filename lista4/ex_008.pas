5b380e745a3674c51f941d375547570d5f11d153
Thalita Nascimento <92101198+thamnasc@users.noreply.github.com>
2022-01-15 22:23:04 -0300

    Update ex_008.pas

--- lista4/ex_008.pas
+++ lista4/ex_008.pas
@@ -1,3 +1,22 @@
+(* Enunciado: 
+
+Faça um programa Pascal que leia do teclado uma sequência de números inteiros até que seja lido um número que seja o dobro ou a 
+metade do anteriormente lido. O programa deve imprimir na saída a quantidade de números lidos, a soma dos números lidos e os dois 
+valores que forçaram a parada do programa. 
+
+Exemplos: 
+Entrada 1: 
+-549 -716 -603 -545 -424 -848 
+
+Saída Esperada 1: 
+6 -3685 -424 -848 
+
+Entrada 2: 
+-549 -716 -603 -545 -424 646 438 892 964 384 192 
+
+Saída Esperada 2: 
+11 679 384 192 *)
+
 program ex_008;
 var num, last, sum: real;
 cont: integer;
